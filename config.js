9aa32169c08ae18b88266779b5d67eb832722c24
Shosuke-kun <132457111+Shosuke-kun@users.noreply.github.com>
2023-11-28 07:26:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['263719647303', 'Mr Frank', true],
-  ['918777033301', 'Piyush Sarkar', true], 
+  ['263785700683', 'Shosuke', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['263719647303','918777033301'] 
-global.prems = ['263719647303', '918777033301', '919398758484']
-global.allowed = ['263719647303', '918777033301', '19152999993']
+global.mods = ['263719647303','263785700683'] 
+global.prems = ['263719647303', '263785700683']
+global.allowed = ['263719647303', '263785700683']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'Automate - Me⚡'
+global.botname = 'Yamamoto⚡'
 global.premium = 'true'
 global.packname = 'Sticker By' 
-global.author = 'Piyush Sarker' 
+global.author = 'Yamamoto' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/the_cyber_punkk\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
